a06a93b4afc348df24a0f044ca79872c87c90066
kunal kewat <60972748+kkewat@users.noreply.github.com>
2024-04-09 21:09:31 +0530

    Create Malt_Parsing.py

---
+++ Malt_Parsing.py
@@ -0,0 +1,5 @@
+from nltk.parse import malt
+mp = malt.MaltParser('maltparser-1.7.2', 'engmalt.linear-1.7.mco')#file	
+t = mp.parse_one('I saw a bird from my window.'.split()).tree()
+print(t)
+t.draw()
